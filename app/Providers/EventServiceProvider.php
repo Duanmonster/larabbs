f929e630433a2293187dbbb8722df4cb8f952cb4
dale <duansemail@qq.com>
2019-12-31 12:47:38 +0800

    两种重置密码成功后的消息提示

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Auth\Events\Registered;
+use Illuminate\Auth\Events\PasswordReset;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Event;
@@ -20,6 +21,9 @@ class EventServiceProvider extends ServiceProvider
         ],
         \Illuminate\Auth\Events\Verified::class => [
             \App\Listeners\EmailVerified::class,
+        ],
+        PasswordReset::class => [
+            \App\Listeners\PasswordRested::class,
         ],
     ];
 
